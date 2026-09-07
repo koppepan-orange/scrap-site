f59cf2bd2e5226543727c2a32c6fe5e016ade823
koppepan_orange <165629784+koppepan-orange@users.noreply.github.com>
2026-09-07 14:29:23 +0900

    togetherいこかな

---
+++ drink_shop/data.js
@@ -0,0 +1,14 @@
+const Drinks = [
+    {
+        name:"water",
+        jpnm:"水",
+        from:["water"],
+        price:110
+    },
+    {
+        name:"milk",
+        jpnm:"牛乳",
+        from:["milk"],
+        price:198
+    }
+]
